9b625700a20ad7467346a5611d1db1aad388c521
Contre <lucascontre95@gmail.com>
2025-07-06 15:45:25 +0200

    fix(tex): fontawesome

--- build-cv.sh
+++ build-cv.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-podman container run --rm -it -v $(pwd):/workdir danteev/texlive make cv.pdf
+
+# Run the container and install fontawesome6 if missing, then compile the CV
+podman container run --rm -it -v "$(pwd)":/workdir danteev/texlive \
+  sh -c "tlmgr update --self && tlmgr install fontawesome6  && make cv.pdf"
